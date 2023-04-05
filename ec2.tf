32881c47a316f2d90220ca53a236ae05854bbcc1
23ArushiJain <87538941+23ArushiJain@users.noreply.github.com>
2023-04-05 16:48:36 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "iac_in_action" {
   key_name = aws_key_pair.iac_in_action.key_name
  
   // dynamically set Security Group ID (firewall)
-  vpc_security_group_ids = [aws_security_group.iac_in_action.id]
+  // vpc_security_group_ids = [aws_security_group.iac_in_action.id]
  
   tags = {
     Name = "Terraform-managed EC2 Instance for IaC in Action"
